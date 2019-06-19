53c79fc9bb36b8ba7d91dbcb696e3fa71948d57e
sjransom <sjransom@gmail.com>
2019-06-19 13:04:18 +0100

    adding local storage to preserve order

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,21 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match
+    // first reinstate the localstorage
+    const localStorageRef = localStorage.getItem(params.storeId)
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) })
+    }
+    console.log(localStorageRef)
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     })
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.order)
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
   }
 
   componentWillUnmount() {
